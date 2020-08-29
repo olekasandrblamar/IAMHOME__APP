55b772e28a570063c974f6cad1005fdaf31cf8c2
olekasandrblamar <olekasandr.blamar@gmail.com>
2020-08-29 11:36:33 -0400

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -110,7 +110,7 @@ class _MyAppState extends State<MyApp> {
             // darkTheme: ThemeData.dark(),
             // theme: myTheme,
             theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
+            // darkTheme: AppTheme.darkTheme,
             initialRoute: routes.RootRoute,
             home: _buildHomeWidget(auth),
             navigatorKey: NavigationService.navigatorKey,
