6e734dddfb678e4408f56d075adc75bfac164402
olekasandrblamar <olekasandr.blamar@gmail.com>
2020-09-29 21:28:45 -0400

    Update main_alpha.dart

--- lib/main_alpha.dart
+++ lib/main_alpha.dart
@@ -12,19 +12,13 @@ import 'config/env.dart';
 
 void main() {
   try {
-    // Set `enableInDevMode` to true to see reports while in debug mode
-    // This is only to be used for confirming that reports are being
-    // submitted as expected. It is not intended to be used for everyday
-    // development.
-    Crashlytics.instance.enableInDevMode = false;
+    WidgetsFlutterBinding.ensureInitialized();
 
     // Pass all uncaught errors from the framework to Crashlytics.
     FlutterError.onError = (FlutterErrorDetails errorDetails) async {
       await FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
       // Forward to original handler.
     };
-
-    WidgetsFlutterBinding.ensureInitialized();
 
     BuildEnvironment.init(
       flavor: BuildFlavor.alpha,
