7d5c29fde49c9a384cf083e3fea3428e439d7675
olekasandrblamar <olekasandr.blamar@gmail.com>
2021-03-11 16:55:40 -0500

    Update apppermissions_widget.dart

--- lib/widgets/apppermissions_widget.dart
+++ lib/widgets/apppermissions_widget.dart
@@ -113,9 +113,12 @@ class _PermissionState extends State<PermissionWidget> {
             : await Permission.locationAlways.request();
 
         if (Platform.isAndroid) {
-          var alwaysStatus = status == PermissionStatus.granted;
-          var inUseStatus = status == PermissionStatus.granted;
-          var locationStatus = status == PermissionStatus.granted;
+          var alwaysStatus = (await Permission.locationAlways.status) ==
+              PermissionStatus.granted;
+          var inUseStatus = (await Permission.locationWhenInUse.status) ==
+              PermissionStatus.granted;
+          var locationStatus =
+              (await Permission.location.status) == PermissionStatus.granted;
 
           if (locationStatus || inUseStatus || alwaysStatus) {
             setState(() {
