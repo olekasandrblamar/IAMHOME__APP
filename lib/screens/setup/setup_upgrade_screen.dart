f6d9827e8c61b2b783f6cd045dd3541cc65e3de5
olekasandrblamar <olekasandr.blamar@gmail.com>
2021-04-08 13:54:52 -0400

    Update setup_upgrade_screen.dart

--- lib/screens/setup/setup_upgrade_screen.dart
+++ lib/screens/setup/setup_upgrade_screen.dart
@@ -109,84 +109,87 @@ class _SetupUpgradeScreenState extends State<SetupUpgradeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Upgrading Device'),
-      // ),
-      backgroundColor: AppTheme.white,
-      body: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 5.0),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    constraints: BoxConstraints(
-                      maxHeight: 300.0,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        // appBar: AppBar(
+        //   title: Text('Upgrading Device'),
+        // ),
+        backgroundColor: AppTheme.white,
+        body: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 5.0),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      constraints: BoxConstraints(
+                        maxHeight: 300.0,
+                      ),
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset(
+                        'assets/images/upgrading.png',
+                      ),
                     ),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Image.asset(
-                      'assets/images/upgrading.png',
+                    SizedBox(
+                      height: 25,
                     ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      'Upgrade in progress',
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: AppTheme.title,
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Upgrade in progress',
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                        style: AppTheme.title,
+                      ),
                     ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      // vertical: 5.0,
-                      horizontal: 35.0,
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        // vertical: 5.0,
+                        horizontal: 35.0,
+                      ),
+                      child: Text(
+                        'Please do not close the app',
+                        textAlign: TextAlign.center,
+                        style: AppTheme.subtitle,
+                      ),
                     ),
-                    child: Text(
-                      'Please do not close the app',
-                      textAlign: TextAlign.center,
-                      style: AppTheme.subtitle,
+                    SizedBox(
+                      height: 25,
                     ),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  !isUpgrading
-                      ? Container(
-                          width: 200,
-                          height: 75,
-                          padding: EdgeInsets.all(10),
-                          child: RaisedButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(4.5),
-                            ),
-                            color: Theme.of(context).primaryColor,
-                            textColor: Colors.white,
-                            onPressed: () => _upgradeDevice(),
-                            child: Text(
-                              'Retry Upgrade',
-                              style: TextStyle(
-                                fontSize: 14,
+                    !isUpgrading
+                        ? Container(
+                            width: 200,
+                            height: 75,
+                            padding: EdgeInsets.all(10),
+                            child: RaisedButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4.5),
+                              ),
+                              color: Theme.of(context).primaryColor,
+                              textColor: Colors.white,
+                              onPressed: () => _upgradeDevice(),
+                              child: Text(
+                                'Retry Upgrade',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
                               ),
                             ),
+                          )
+                        : Container(
+                            height: 0,
                           ),
-                        )
-                      : Container(
-                          height: 0,
-                        ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
