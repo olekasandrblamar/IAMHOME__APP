f3bb60c8826168a2edbdda72d8eeaffe6d9c8162
olekasandrblamar <olekasandr.blamar@gmail.com>
2021-07-13 11:41:17 -0400

    Updated code for auto update

--- lib/screens/setup/setup_home_screen.dart
+++ lib/screens/setup/setup_home_screen.dart
@@ -148,52 +148,61 @@ class _SetupHomeScreenState extends State<SetupHomeScreen>
 
   void updateVersionCheck() async {
     final prefs = await SharedPreferences.getInstance();
-    var versionCheckDate = await prefs.getString('versionCheckDate');
+    var versionCheckDateInMillis = await prefs.getString('versionCheckDate');
 
-    // DateTime d = DateTime.now();
+    var currentTimeInMillis = DateTime.now().millisecondsSinceEpoch;
+    var checkAndValidate = true;
+    //if the prev version date doesn't exist set that value
+    if (versionCheckDateInMillis == null) {
+      await prefs.setString('versionCheckDate', currentTimeInMillis.toString());
+      checkAndValidate = true;
+    }else{
+      var milliSecondsSinceLastCheck = DateTime.now().millisecondsSinceEpoch - int.parse(versionCheckDateInMillis);
+      var dayInMillis = 1000*60*60*24;
+      var daysSinceLastUpdate = milliSecondsSinceLastCheck / dayInMillis;
 
-    // var currentDate = d.setDate(d.getDate());
-    // if (versionCheckDate == null) {
-    //   await prefs.setString('versionCheckDate', currentDate.toString());
-    // }
+      //if the last validation is more than 3 days ago
+      if(daysSinceLastUpdate > 3.0){
+        checkAndValidate = true;
+        await prefs.setString('versionCheckDate', currentTimeInMillis.toString());
+      }
+    }
 
-    // // var daysFromNow: any = d.setDate(d.getDate() - 3); // three days fromNow
-    // var millisec = currentDate - DateTime(int.parse(versionCheckDate));
-    // var seconds = double.parse((millisec / 1000).toFixed(0));
-
-    // /* Checking if current date is greater than 3 days
-    //             and if it is greater than 3 days check fior update
-    //     */
-    // if (seconds > 259200) {
-    //   await prefs.setString('versionCheckDate', currentDate.toString());
-    //   await appVersionCheck();
-    // }
+    if (checkAndValidate) {
+      await appVersionCheck();
+    }
   }
 
   Future<void> appVersionCheck() async {
     final PackageInfo packageInfo = await PackageInfo.fromPlatform();
     final currentVersion = packageInfo.version;
 
+    //Get the current version from the API
     var versionData = await Provider.of<AuthProvider>(context, listen: false)
         .checkAppVersion();
 
+    //if the version data exists
     if (versionData.isNotEmpty) {
-      // var _currentVersion =
-      //     JSON.stringify(currentVersion.toString().split('.'));
-      // var _latestVersioniOS =
-      //     JSON.stringify(versionData.ios.toString().split('.'));
-      // var _latestVersionAndroid =
-      //     JSON.stringify(versionData.android.toString().split('.'));
+      var _currentVersion =
+          currentVersion.toString().split('.').map((e) => int.parse(e)).toList();
 
-      // if (Platform.isIOS) {
-      //   if (_latestVersioniOS > _currentVersion) {
-      //     updateApp();
-      //   }
-      // } else {
-      //   if (_latestVersionAndroid > _currentVersion) {
-      //     updateApp();
-      //   }
-      // }
+      //default the version to android
+      var curStoreVersion = versionData.android.toString();
+
+      //If the current platform is IOS get the is version
+      if (Platform.isIOS) {
+        curStoreVersion = versionData.ios.toString();
+      }
+
+      //Split the version into major.minor.build
+      var _latestStoreVersion = curStoreVersion.split('.').map((e) => int.parse(e)).toList();
+
+      //Compare the versions and if the new version os greater than the current version update the app
+      if (_latestStoreVersion[0] > _currentVersion[0] ||
+          _latestStoreVersion[1] > _currentVersion[1] ||
+          _latestStoreVersion[2] > _currentVersion[2]) {
+        updateApp();
+      }
     }
   }
 
