3e7025a4b194ed9cc36d9db576c39c9f142782f9
olekasandrblamar <olekasandr.blamar@gmail.com>
2021-07-15 14:31:54 -0400

    Updated the code for checking device status

--- lib/screens/setup/setup_home_screen.dart
+++ lib/screens/setup/setup_home_screen.dart
@@ -15,7 +15,6 @@ import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:package_info/package_info.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
