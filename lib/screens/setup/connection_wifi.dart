85a6e853d306b16477872bdeccd3fea1a65f3433
olekasandrblamar <olekasandr.blamar@gmail.com>
2021-10-27 09:29:51 -0400

    Updated code for password

--- lib/screens/setup/connection_wifi.dart
+++ lib/screens/setup/connection_wifi.dart
@@ -458,30 +458,7 @@ class _ConnectionWifiScreenState extends State<ConnectionWifiScreen>
                     SizedBox(
                       height: 30,
                     ),
-                    TextFormField(
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                        // suffixIcon: Icon(Icons.remove_red_eye),
-                        decoration: _inputDecoration('Password', ''),
-                        controller: _passwordController,
-                        focusNode: _passwordFocusNode,
-                        keyboardType: TextInputType.text,
-                        textInputAction: TextInputAction.done,
-                        obscureText: _showPassword,
-                        autofocus: true,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter password.';
-                          }
-
-                          if (value.length < 3) {
-                            return 'password must be more than 2 charater';
-                          }
-
-                          return null;
-                        },
-                        onSaved: (password) => _password = password),
+                    _passwordInput(),
                     SizedBox(
                       height: 50,
                     ),
@@ -538,6 +515,35 @@ class _ConnectionWifiScreenState extends State<ConnectionWifiScreen>
           ),
         ],
       ),
+    );
+  }
+
+  Widget _passwordInput() {
+    return TextFormField(
+      style: TextStyle(
+        fontSize: 24,
+      ),
+      // suffixIcon: Icon(Icons.remove_red_eye),
+      decoration: _inputDecoration('Password', ''),
+      controller: _passwordController,
+      focusNode: _passwordFocusNode,
+      keyboardType: TextInputType.text,
+      textInputAction: TextInputAction.done,
+      obscureText: _showPassword,
+      autofocus: false,
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter password.';
+        }
+
+        if (value.length < 3) {
+          return 'password must be more than 2 charater';
+        }
+
+        return null;
+      },
+      onSaved: (password) => _password = password,
+      // onChanged: onChangePhoneNumberInput,
     );
   }
 
