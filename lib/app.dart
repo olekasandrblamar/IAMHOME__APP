26bccbd0e1b140f52f0674d38c44ed7331997a98
olekasandrblamar <olekasandr.blamar@gmail.com>
2022-11-07 12:57:00 -0500

    terra api first commit

--- lib/app.dart
+++ lib/app.dart
@@ -28,6 +28,9 @@ import 'router.dart' as router;
 import 'screens/setup/setup_home_screen.dart';
 import 'theme.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:terra_flutter_bridge/terra_flutter_bridge.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 // import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 // import 'config/locator.dart';
 
@@ -39,6 +42,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final AppLanguageProvider appLanguage = AppLanguageProvider();
   static FirebaseAnalytics analytics = FirebaseAnalytics();
+  //////////////For Terra API///////////////////
+  bool _initialised = false;
+  bool _connected = false;
+  bool _daily = false;
+  String _testText = "Hello World";
+  ////////////////////////////////////////////////
 
   @override
   void initState() {
@@ -47,8 +56,145 @@ class _MyAppState extends State<MyApp> {
     _handleStartUpLogic();
 
     appLanguage.fetchLocale();
-  }
 
+    ///////////For Terra API////////////////////////////////
+    initTerraFunctionState();
+    ///////////////////////////////////////////////////
+
+  }
+  ///////////////For Terra API /////////////////////
+  Future<void> initTerraFunctionState() async {
+    bool initialised = false;
+    bool connected = false;
+    bool daily = false;
+    String testText;
+    Connection c = Connection.samsung;
+    /*
+    To use the Samsung integration, the user needs Health Platform downloaded on their device and their Samsung Health Account linked to Health Platform. This can be done by going on Samsung Health -> Profile -> Settings -> Connected Services -> Health Platform and giving Health Platform access to their data.
+    */
+
+    // Function messages may fail, so we use a try/catch Exception.
+    // We also handle the message potentially returning null.
+    // USE YOUR OWN CATCH BLOCKS
+    // HAVING ALL FUNCTIONS IN THE SAME CATCH IS NOT A GOOD IDEA
+    try {
+      DateTime now = DateTime.now().toUtc();
+      DateTime lastMidnight = DateTime(now.year, now.month, now.day);
+      Fluttertoast.showToast(
+        msg: "trying integration init",
+        toastLength: Toast.LENGTH_SHORT,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+      initialised =
+          await TerraFlutter.initTerra("ceras-dev-y5kN5MDRKv", "67d93d7e-09f1-4402-b5ad-fb437f3b4628") ??
+              false;
+      String str;
+      if(_initialised) str = "true";
+      else str = "false";
+      Fluttertoast.showToast(
+        msg: "Did integration init:" + str,
+        toastLength: Toast.LENGTH_SHORT,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+
+
+      connected = await TerraFlutter.initConnection(c, "a3e614f4481dbb92cca6d2957bde3f71951551e726710c2f7b88d7c7c5174562", false, []) ??
+          false;
+
+      if(_connected) str = "true";
+      else str = "false";
+      Fluttertoast.showToast(
+        msg: "Is integration connected:" + str,
+        toastLength: Toast.LENGTH_SHORT,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+
+      testText = await TerraFlutter.getUserId(c) ?? "1234";
+
+      Fluttertoast.showToast(
+        msg: "User id:" + testText,
+        toastLength: Toast.LENGTH_SHORT,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+
+      daily = await TerraFlutter.getDaily(
+          c, lastMidnight, now) ??
+          false;
+      daily = await TerraFlutter.getAthlete(c) ?? false;
+      daily = await TerraFlutter.getMenstruation(
+          c, DateTime(2022, 9, 25), DateTime(2022, 9, 30)) ??
+          false;
+      daily = await TerraFlutter.getNutrition(
+          c, DateTime(2022, 7, 25), DateTime(2022, 7, 26)) ??
+          false;
+      daily = await TerraFlutter.getSleep(
+          c, now.subtract(Duration(days: 1)), now) ??
+          false;
+      daily = await TerraFlutter.getActivity(
+          c, DateTime(2022, 7, 25), DateTime(2022, 7, 26)) ??
+          false;
+
+      if(_daily) str = "true";
+      else str = "false";
+      Fluttertoast.showToast(
+        msg: "Requested daily webhook for integration:" + str,
+        toastLength: Toast.LENGTH_SHORT,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    } on Exception catch (e) {
+      // print('error caught: $e');
+      testText = "Some exception went wrong";
+      initialised = false;
+      connected = false;
+      daily = false;
+      Fluttertoast.showToast(
+        msg: testText,
+        toastLength: Toast.LENGTH_SHORT,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.black,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+
+    }
+    Fluttertoast.showToast(
+      msg: "No exception occured",
+      toastLength: Toast.LENGTH_SHORT,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _initialised = initialised;
+      _connected = connected;
+      _daily = daily;
+      _testText = testText;
+    });
+
+
+  }
+  /////////////////////////////////////////////////////////
   Future<void> _handleStartUpLogic() async {
     await _initializeFlutterFire();
 
